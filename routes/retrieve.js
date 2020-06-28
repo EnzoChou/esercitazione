97ce4c87e9ce0359899e3af579bbd07751ba9004
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-06-28 14:35:49 +0200

    aggiunta la retrieveById
    aggiunta la funzione per trovare persone dall'id

--- routes/retrieve.js
+++ routes/retrieve.js
@@ -23,4 +23,28 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/:personId', (req, res, next) => {
+  var id = req.params.personId;
+  connection.connect();
+  personModel.findById(id)
+  .exec()
+  .then( doc => {
+    connection.disconnect();
+    console.log("from database", doc )
+    if ( doc != undefined ) {
+      res.status(200).json(doc);
+    } else {
+      res.status(404).json({
+        message : "no valid entry provided;"
+      });
+    };
+  })
+  .catch( err => {
+    connection.disconnect();
+    res.status(500).json( {
+      error : err
+    });
+  });
+});
+
 module.exports = router;
