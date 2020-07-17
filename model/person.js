f0bf9ad0400072e4f12d5813aeedda02fdd6d547
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-07-17 12:13:25 +0200

    aggiunti schema joi
    aggiunti schema joi per la validazione dell'oggetto persona,
    per i parametri della pagina durante la richiesta della retrieveALL
    per il controllo dell'id nella ricerca singola retrieveONE

--- model/person.js
+++ model/person.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var Joi = require("joi");
 
 //schema della persona
 var personSchema = new mongoose.Schema({
@@ -19,4 +20,27 @@ var personSchema = new mongoose.Schema({
 
 });
 
-module.exports = mongoose.model( 'Persona', personSchema, 'Persone' );
+//module.exports = mongoose.model( 'Persona', personSchema, 'Persone' );
+
+var schemas = {
+  person: Joi.object().keys({
+    name: Joi.string().regex(/^[a-zA-Z]{3,30}$/).required(),
+    surname: Joi.string().regex(/^[a-zA-Z]{3,30}$/).required(),
+    birthplace: Joi.string(),
+    birthdate: Joi.date()
+  }),
+  personLIST: Joi.object().keys({
+    page: Joi.number().required(),
+    pageSize: Joi.number().required()
+  }),
+  personId: Joi.object().keys({
+    personId: Joi.string().alphanum().min(1).required()
+  })
+  // define other schemas the same way as person
+
+};
+
+module.exports = {
+  person: mongoose.model( 'Persona', personSchema, 'Persone' ),
+  schemas: schemas
+};
