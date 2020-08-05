aab2fbbc8b29ca8afd6b2975df65616916182584
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-08-05 17:10:23 +0200

    aggiornato lo script di savePersone
    aggiornato lo script di savePersone aggiungendo i valori di subscription(true,false) e status(single, engaged, married)

--- scripts/savePersone.js
+++ scripts/savePersone.js
@@ -14,7 +14,11 @@ function uncapitalize(word) {
   return word.charAt(0).toLowerCase() + word.slice(1);
 }
 
-var numeroDiPersoneDaSalvareNelDB = 500;
+function randomize(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var numeroDiPersoneDaSalvareNelDB = 50;
 
 var persone = [];
 var name = ["Andrea","Brandon","Carla",
@@ -23,18 +27,24 @@ var name = ["Andrea","Brandon","Carla",
 "Paola","Quentin","Roberta","Sabrina","Tano","Ugo",
 "Valeria","Zarathustra"];
 var surname = ["Berardino", "Annunzio", "Bonaventura",
- "Cintio","Amario", "Intino", "Prinzio", "Addario",
-  "Ascenzo", "Credico", "Ciuffini", "Ciocca", "Pelliccione",
-  "Scarsella", "Cocciolone", "Cetrullo", "Scimia", "Mammarella", "Zappacosta"];
+"Cintio","Amario", "Intino", "Prinzio", "Addario",
+"Ascenzo", "Credico", "Ciuffini", "Ciocca", "Pelliccione",
+"Scarsella", "Cocciolone", "Cetrullo", "Scimia", "Mammarella", "Zappacosta"];
+var subscription = [true, false];
+var status = ["single", "engaged", "married"]
 
 function aggiungiPersone(n) {
   var i = 0;
   while(i<n) {
-    let randomName = capitalize(name[Math.floor(Math.random() * name.length)]);
-    let randomSurname = capitalize(surname[Math.floor(Math.random() * surname.length)]);
+    let randomName = capitalize(randomize(name));
+    let randomSurname = capitalize(randomize(surname));
+    let randomSubscription = randomize(subscription);
+    let randomStatus = randomize(status);
     var persona = new Persona({
       name : randomName,
-      surname : randomSurname
+      surname : randomSurname,
+      subscription : randomSubscription,
+      status : randomStatus
     });
     persone.push(persona);
     i++;
@@ -49,7 +59,7 @@ function salvaColMap(persone){
     console.log("persona: ", elem);
     return elem.save();
   }).then( result => {
-    console.log('result', result);
+    console.log('ritorno then di salvaColMap', result);
     db_yeah.close();
   })
   .catch(err =>{
@@ -62,18 +72,18 @@ function salvaColMap(persone){
 function salvaColAll(persone){
   var promises = [];
   persone.forEach(ciccino => {
-      var ret = ciccino.save();
-      promises.push(ret);
-      console.log(ret)
+    var ret = ciccino.save();
+    promises.push(ret);
+    console.log(ret)
   });
   Promise.all(promises)
   .then( ret => {
-    console.log('ritorno then', ret);
+    console.log('ritorno then di salvaColAll', ret);
     db_yeah.close();
-    })
-    .catch(err => {
-      console.log('err', err);
-      db_yeah.close()
-    });
+  })
+  .catch(err => {
+    console.log('err', err);
+    db_yeah.close()
+  });
 };
 salvaColMap(persone);
