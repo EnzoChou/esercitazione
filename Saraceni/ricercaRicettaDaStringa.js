54b9ebe176c688f901ae4902dfe2c0ec357c5243
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-16 11:16:19 +0200

    matchIngredientiPrincipali
    aggiunta funzione per il match di ingredienti principali e aggiunta della chiave match con la probabilità di matching

--- Saraceni/ricercaRicettaDaStringa.js
+++ Saraceni/ricercaRicettaDaStringa.js
@@ -1,12 +1,40 @@
 var strutture = require('./creazioneListeDaFileJson');
 var natural = require('natural');
+var classifier = require('./trainParole');
 var tokenizer = new natural.WordTokenizer();
-natural.PorterStemmerIt.attach();
+natural.PorterStemmer.attach(); //english language set -> 'words'.tokenizeAndStem() toSingularizeAndTurnIntoArrayOfWords
+
+var TfIdf = natural.TfIdf;
+var tfIdf = new TfIdf(); //frequency control -> tfIdf.addDocument('stringData') ;; tfIdf.tfidfs('wordToMeasure', function(document#, numberOfRepetition){courseOfAction})
 
 var listaRicette = strutture.listaRicette;
 var listaIngredientiPrincipali = strutture.listaIngredientiPrincipali;
 var listaIngredientiSecondari = strutture.listaIngredientiSecondari;
 var listaVini = strutture.listaVini;
+var listaParoleChiave = strutture.listaParoleChiave;
 
-var utente = "ciao, stasera ho voglia di pollo!";
-console.log(utente);
+var utente = "i'd like some chicken nuggets and pastrami";
+var stemmed = utente.stem();
+var tokenized = tokenizer.tokenize(stemmed);
+var tokenizeAndStem = utente.tokenizeAndStem();
+//console.log(natural.PorterStemmer.stem(utente));
+//console.log(natural.JaroWinklerDistance("dixon","dicksonx", undefined, true));
+//console.log('tokenize',tokenized);
+//console.log('stemmed',stemmed);
+//console.log('tokenizeAndStem',tokenizeAndStem);
+//console.log(listaIngredientiPrincipali);
+var paroleDaCercare = utente.tokenizeAndStem();
+
+var matchIngredientiPrincipali = listaIngredientiPrincipali.filter(function(ingrediente){
+  return paroleDaCercare.find(function(parola){
+    if(natural.JaroWinklerDistance(ingrediente.nome,parola,undefined,true)>0.8) {
+      ingrediente.match = natural.JaroWinklerDistance(ingrediente.nome,parola,undefined,true);
+      return true;
+    }
+    return false;
+  })
+})
+
+matchIngredientiPrincipali.sort(function(a,b){return b.match - a.match});
+
+console.log(matchIngredientiPrincipali);
