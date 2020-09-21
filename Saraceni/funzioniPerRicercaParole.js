5f4f1ce6e306d7804adb9f736504605464eca4cd
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-21 14:26:48 +0200

    aggiunto indicemassimoaccoppiamento
    aggiunto metodo indicemassimoaccoppiamento

--- Saraceni/funzioniPerRicercaParole.js
+++ Saraceni/funzioniPerRicercaParole.js
@@ -1,37 +1,57 @@
-var natural = require('natural');
-var funzioniPerRicercaParole = {};
+var natural = require('natural')
+var funzioniPerRicercaParole = {}
 
-var ricetteDaIDs = function(listaID,listaRicette) {
-  return listaID.filter(id => listaRicette.id.find(idRicetta => id == idRicetta))
+var ricetteDaIDs = function (listaID, listaRicette) {
+  return listaID.filter(id => listaRicette.id.find(idRicetta => id === idRicetta))
 }
 
-var ricetteDaIngrediente = function(ingrediente,listaRicette) {
-  return ingrediente.ricette.map(codiceRicetta => listaRicette.find(ricetta => ricetta.id == codiceRicetta ))
+var ricetteDaIngrediente = function (ingrediente, listaRicette) {
+  return ingrediente.ricette.map(codiceRicetta => listaRicette.find(ricetta => ricetta.id === codiceRicetta))
 }
 
-var ricetteDaIngredienti = function(ingredienti,listaRicette) {
-  var arrayRicette = [];
+var ricetteDaIngredienti = function (ingredienti, listaRicette) {
+  var arrayRicette = []
   ingredienti.forEach((item, i) => {
-    arrayRicette = arrayRicette.concat(ricetteDaIngrediente(item,listaRicette));
-  });
-  return arrayRicette;
+    arrayRicette = arrayRicette.concat(ricetteDaIngrediente(item, listaRicette))
+  })
+  return arrayRicette
 }
 
-var somiglianzaParoleArray = function(arrayParole,arrayRicette) {
-  var contatore = 0;
+var somiglianzaParoleArray = function (arrayParole, arrayDiConfronto) {
+  var contatore = 0
   arrayParole.forEach((item, i) => {
-    arrayRicette.forEach((item2, j) => {
-      if(natural.JaroWinklerDistance(item,item2,undefined,true)>0.8){
-        contatore+=natural.JaroWinklerDistance(item,item2,undefined,true);
+    arrayDiConfronto.forEach((item2, j) => {
+      if (natural.JaroWinklerDistance(item, item2, undefined, true) > 0.8) {
+        console.log('confronto parole', item, item2, natural.JaroWinklerDistance(item, item2, undefined, true))
+        contatore += natural.JaroWinklerDistance(item, item2, undefined, true)
       }
-    });
-  });
-  return contatore/(arrayRicette.length);
+    })
+  })
+  return contatore / (arrayDiConfronto.length)
 }
 
-funzioniPerRicercaParole.ricetteDaIDs = ricetteDaIDs;
-funzioniPerRicercaParole.ricetteDaIngrediente = ricetteDaIngrediente;
-funzioniPerRicercaParole.ricetteDaIngredienti = ricetteDaIngredienti;
-funzioniPerRicercaParole.somiglianzaParoleArray = somiglianzaParoleArray;
+var indiceMassimoAccoppiamento = function (arr) {
+  if (arr.length === 0) {
+    return -1
+  }
 
-module.exports = funzioniPerRicercaParole;
+  var max = arr[0]
+  var maxIndex = 0
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      maxIndex = i
+      max = arr[i]
+    }
+  }
+
+  return maxIndex
+}
+
+funzioniPerRicercaParole.ricetteDaIDs = ricetteDaIDs
+funzioniPerRicercaParole.ricetteDaIngrediente = ricetteDaIngrediente
+funzioniPerRicercaParole.ricetteDaIngredienti = ricetteDaIngredienti
+funzioniPerRicercaParole.somiglianzaParoleArray = somiglianzaParoleArray
+funzioniPerRicercaParole.indiceMassimoAccoppiamento = indiceMassimoAccoppiamento
+
+module.exports = funzioniPerRicercaParole
