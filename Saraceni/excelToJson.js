9b80002b250c1aded4d9f6988c424029f2e1659a
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-21 14:27:05 +0200

    rimossi i semicolon
    rimossi i semicolon

--- Saraceni/excelToJson.js
+++ Saraceni/excelToJson.js
@@ -1,10 +1,10 @@
-'use strict';
-const excelToJson = require('convert-excel-to-json');
+'use strict'
+const excelToJson = require('convert-excel-to-json')
 
 const result = excelToJson({
   sourceFile: '../Ricette per Foodpairing + ricette italiane_25_8.xlsx'
-});
+})
 
-//console.log(result);
+// console.log(result);
 
-module.exports = result;
+module.exports = result
