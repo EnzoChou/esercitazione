8a4c107ff975558a7cca4823a210b35e3fc8df6f
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-21 14:25:19 +0200

    non è cambiato nulla
    non è cambiato nulla

--- Saraceni/trainParole.js
+++ Saraceni/trainParole.js
@@ -1,22 +1,21 @@
-var natural = require('natural');
-var classifier = new natural.BayesClassifier(); // classifier.addDocument('text','label') ;; classifier.train() ;; 'labelGuess' = classifier.classify('text')
-                                                // [{label:'word',value:'percentageOfCertainty'},{...}] = classifier.getClassifications('text')
-//natural.JaroWinklerDistance('string1','string2') -> calculate the word difference from 0 to 1
-natural.PorterStemmer.attach();
-var strutture = require('./creazioneListeDaFileJson');
-var listaParoleChiave = strutture.listaParoleChiave;
-var listaParoleChiavePerCategoria = strutture.listaParoleChiavePerCategoria;
+var natural = require('natural')
+var classifier = new natural.BayesClassifier() // classifier.addDocument('text','label') ;; classifier.train() ;; 'labelGuess' = classifier.classify('text')
+// [{label:'word',value:'percentageOfCertainty'},{...}] = classifier.getClassifications('text')
+// natural.JaroWinklerDistance('string1','string2') -> calculate the word difference from 0 to 1
+natural.PorterStemmer.attach()
+var strutture = require('./creazioneListeDaFileJson')
+var listaParoleChiave = strutture.listaParoleChiave
+var listaParoleChiavePerCategoria = strutture.listaParoleChiavePerCategoria
 
+var antipastiContorni = listaParoleChiavePerCategoria.antipastiContorni.map(parola => parola.stem())
+var primi = listaParoleChiavePerCategoria.primi.map(parola => parola.stem())
+var secondi = listaParoleChiavePerCategoria.secondi.map(parola => parola.stem())
+var ingredientiPrincipali = listaParoleChiavePerCategoria.ingredientiPrincipali.map(parola => parola.stem())
+var ingredientiSecondari = listaParoleChiavePerCategoria.ingredientiPrincipali.map(parola => parola.stem())
 
-var antipastiContorni = listaParoleChiavePerCategoria.antipastiContorni.map(parola => parola.stem());
-var primi = listaParoleChiavePerCategoria.primi.map(parola => parola.stem());
-var secondi = listaParoleChiavePerCategoria.secondi.map(parola => parola.stem());
-var ingredientiPrincipali = listaParoleChiavePerCategoria.ingredientiPrincipali.map(parola => parola.stem());
-var ingredientiSecondari = listaParoleChiavePerCategoria.ingredientiPrincipali.map(parola => parola.stem());
+classifier.addDocument('chicken', 'chicken')
+classifier.addDocument('cihken', 'chicken')
 
-classifier.addDocument('chicken','chicken');
-classifier.addDocument('cihken','chicken');
-
-classifier.train();
-//console.log(classifier.getClassifications('wanna some chichen today?'));
-module.exports = classifier;
+classifier.train()
+// console.log(classifier.getClassifications('wanna some chichen today?'));
+module.exports = classifier
