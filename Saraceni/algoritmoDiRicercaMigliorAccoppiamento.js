9635594d007a4bdda66802f09ef6bced414a9e07
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-21 16:02:03 +0200

    messo a punto decisione algoritmo
    messo a punto decisione algoritmo

--- Saraceni/algoritmoDiRicercaMigliorAccoppiamento.js
+++ Saraceni/algoritmoDiRicercaMigliorAccoppiamento.js
@@ -1,9 +1,15 @@
 var fun = require('./funzioniPerRicercaParole')
 var natural = require('natural')
 var strutture = require('./creazioneListeDaFileJson')
+var metodoDiRicerca = require('./ricercaRicettaDaStringa')
 natural.PorterStemmer.attach() // english language set -> 'words'.tokenizeAndStem() toSingularizeAndTurnIntoArrayOfWords
 
 var listaRicette = strutture.listaRicette
+var listaParoleChiavePerCategoria = strutture.listaParoleChiavePerCategoria
+var listaIngredientiPrincipali = strutture.listaIngredientiPrincipali
+var listaIngredientiSecondari = strutture.listaIngredientiSecondari
+var listaVini = strutture.listaVini
+var listaParoleChiave = strutture.listaParoleChiave
 var listaParoleChiavePerCategoria = strutture.listaParoleChiavePerCategoria
 
 var nomiRicette = listaRicette.map(ricetta => ricetta.nome.tokenizeAndStem())
@@ -11,7 +17,7 @@ var ingredientiPrincipali = listaParoleChiavePerCategoria.ingredientiPrincipali.
 var ingredientiSecondari = listaParoleChiavePerCategoria.ingredientiSecondari.map(parola => parola.tokenizeAndStem())
 var listaPortate = ['apetizer', 'first course', 'second course', 'side dish', 'dessert'].map(portata => portata.tokenizeAndStem())
 
-var utente = "i'd like some first curse"
+var utente = "i'd like some avogato tost"
 var paroleDaCercare = utente.tokenizeAndStem()
 
 var arrayPunteggio = [
@@ -21,10 +27,23 @@ var arrayPunteggio = [
   ingredientiSecondari
 ]
 
+var arrayAlgoritmoScelto = {
+  0: metodoDiRicerca.ricettaTrovataDaRicette,
+  2: metodoDiRicerca.ricetteTrovateDaIngredientiPrincipali
+}
+
 var punteggi = arrayPunteggio.map(arrayDiArray =>
   Math.max(...arrayDiArray.map(array =>
     fun.somiglianzaParoleArray(paroleDaCercare, array)
   ))
 )
 
-// console.log(punteggi)
+var indexScelto = punteggi.indexOf(punteggi.find(elem => elem > 0.9))
+
+var metodoScelto = arrayAlgoritmoScelto[indexScelto]
+
+console.log('parole inserite dall\'utente', paroleDaCercare)
+console.log('indice: [nomiRicette, listaPortate, IngPrincipali, ingSecondari]')
+console.log('punteggi', punteggi)
+console.log('index scelto', indexScelto)
+console.log('risultato trovato dal bot', metodoScelto(paroleDaCercare))
