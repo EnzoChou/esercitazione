368e95ce92949631a22a23a02ba91b15d7107b86
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-22 21:55:41 +0200

    isolata richiesta utente
    isolata richiesta utente

---
+++ Saraceni/richiestaUtente.js
@@ -0,0 +1,7 @@
+var natural = require('natural')
+var metodoDiRicerca = require('./ricercaRicettaDaStringa')
+natural.PorterStemmer.attach() // english language set -> 'words'.tokenizeAndStem() toSingularizeAndTurnIntoArrayOfWords
+
+var richiestaUtente = "i'd like some fish"
+
+console.log('risultato trovato dal bot:', metodoDiRicerca.scegliMetodo(richiestaUtente))
