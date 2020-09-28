eecdcee37d2b73ba193b1b1eef06c2c81ffc8139
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-28 11:39:19 +0200

    fix beautify
    fix beautify

--- saraceni_wines/integrations/foodpairing.js
+++ saraceni_wines/integrations/foodpairing.js
@@ -1,35 +1,34 @@
-function processing(user, event, param) {
+function processing (user, event, param) {
   return new Promise((resolve, reject) => {
-    console.log("asdfghjkl foodpairing")
-    console.log("event.message.text", event.message.text);
-    console.log("param", param);
+    console.log('event.message.text', event.message.text);
+    console.log('param', param);
 
     var message_text = event.message.text;
-    console.log("message_text", message_text);
+    console.log('message_text', message_text);
 
     var foodpairing_seach = external_services.saraceni_wines.integrations.ricercaVini;
     var result = foodpairing_seach.metodoScelto(message_text);
 
-    console.log("result", result);
+    console.log('result', result);
     var messages = [{
-      "text": "Ecco a te i vini:"
+      text: 'Ecco a te i vini:'
     }];
     result.forEach(elem => {
       messages.push({
-        title: elem['title'],
+        title: elem.title,
         // subtitle: element['streetAddress'] + ", " + element['locality'],
         // image_url: "https://s3-eu-west-1.amazonaws.com/media-hej/Renault%20Dealer/image/1591968591175.png",
         buttons: [{
-          type: "web_url",
-          title: "SELEZIONA",
-          url: "https://www.saraceniwines.com/"
+          type: 'web_url',
+          title: 'SELEZIONA',
+          url: 'https://www.saraceniwines.com/'
         }]
       });
     });
 
     resolve({
-      'replace': true,
-      'msg': messages
+      replace: true,
+      msg: messages
     });
 
     // return yeah(event)
@@ -55,7 +54,6 @@ function processing(user, event, param) {
     //             });
     //         }
     // });
-
   });
 }
 exports.processing = processing;
