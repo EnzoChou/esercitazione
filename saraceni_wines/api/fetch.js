e97ef9b26421d54679a73b10c0038c43937337b2
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-29 15:13:13 +0200

    wrapped into functions
    fetch and fetchAll wrapped into functions

--- saraceni_wines/api/fetch.js
+++ saraceni_wines/api/fetch.js
@@ -3,10 +3,12 @@ global.fetch = require('node-fetch');
 var Client = require('shopify-buy');
 
 // Initializing a client to return content in the store's primary language
-const client = Client.buildClient({
-  domain: 'test-hej.myshopify.com',
-  storefrontAccessToken: 'ab5232770d3011658fd90b0cf536a8c0'
-});
+const client = function () {
+  Client.buildClient({
+    domain: 'test-hej.myshopify.com',
+    storefrontAccessToken: 'ab5232770d3011658fd90b0cf536a8c0'
+  });;
+}
 
 // console.log("client", client);
 
@@ -15,13 +17,19 @@ var productId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzU3MzMxMjk0NTM3MzQ=';
 var productVariantId;
 var productInfo;
 
-return client.product.fetch(productId).then((product) => {
-    // Do something with the product
-    productInfo = product.id;
-    console.log('product ---> ', product);
-    console.log('productInfo ---> ', productInfo);
-    return;
-  })
-  .catch(error =>
-    console.log('error', error)
-)
+var fetch = function (productId) {
+  client.product.fetch(productId)
+    .then((product) => {
+      // Do something with the product
+      productInfo = product.id;
+      console.log('product ---> ', product);
+      console.log('productInfo ---> ', productInfo);
+      return product;
+    })
+    .catch(error =>{
+      console.log('error', error);
+      return [];
+    })
+};
+
+module.exports = fetch;
--- saraceni_wines/api/fetchAll.js
+++ saraceni_wines/api/fetchAll.js
@@ -6,7 +6,7 @@ var Client = require('shopify-buy');
 const client = Client.buildClient({
   domain: 'test-hej.myshopify.com',
   storefrontAccessToken: 'ab5232770d3011658fd90b0cf536a8c0'
-});
+});;
 
 // console.log("client", client);
 
@@ -15,28 +15,43 @@ var productId;
 var productVariantId;
 var productInfo;
 
+/*
+product.id Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzU3MzMxMjk0NTM3MzQ=
+product.title Vino 1
+product.id Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzU3NzA5ODUwNDYxODI=
+product.title Hugo
+product.id Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzU3NzUzMjc5MjAyOTQ=
+product.title Perlè
+product.id Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzU3NzUzMzAxMTU3NTA=
+product.title Libero Chianti
+*/
+
 
 // Fetch all products in your shop
-return client.product.fetchAll()
-.then((products) => {
-  // Do something with the products
-  productId = products[0].id;
-  productVariantId = products[0].variants[0].id;
-  console.log('number of products ---> ', products.length);
-  console.log('products[0] ---> ', products[0]);
-  console.log('products[0].title --->', products[0].title);
-  console.log('products[0].variants[0] --->', products[0].variants[0]);
-  console.log('products[0].variants[0].id ---> ', products[0].variants[0].id);
-  console.log('productId ---> ', productId);
-  console.log('productId.quantity ---> ', productId.quantity);
-  products.forEach(product => {
-    console.log('single product ---> ', product,
-    '\nproduct.variants.length', product.variants.length,
-    '\nproduct.id', product.id, 
-    '\nproduct.title', product.title);
-  })
-  return;
-})
-.catch(error =>
-  console.log('error', error)
-)
+var fetchAll = function () {
+  return client.product.fetchAll()
+    .then((products) => {
+      // Do something with the products
+      productId = products[0].id;
+      productVariantId = products[0].variants[0].id;
+      console.log('number of products ---> ', products.length);
+      console.log('products[0] ---> ', products[0]);
+      console.log('products[0].title --->', products[0].title);
+      console.log('products[0].variants[0] --->', products[0].variants[0]);
+      console.log('products[0].variants[0].id ---> ', products[0].variants[0].id);
+      console.log('productId ---> ', productId);
+      console.log('productId.quantity ---> ', productId.quantity);
+      products.forEach(product => {
+        console.log('single product ---> ', product,
+          '\nproduct.variants.length', product.variants.length,
+          '\nproduct.id', product.id,
+          '\nproduct.title', product.title);
+      })
+      return products;
+    })
+    .catch(error =>
+      console.log('error', error)
+    )
+}
+
+module.exports = fetchAll;
