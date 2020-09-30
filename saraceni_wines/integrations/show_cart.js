c7ae482fbe8087e69650bb780cbe9e167c2b2927
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-09-30 19:41:41 +0200

    sistemata l'integrazione show_cart
    sistemata l'integrazione show_cart

--- saraceni_wines/integrations/show_cart.js
+++ saraceni_wines/integrations/show_cart.js
@@ -1,31 +1,41 @@
+var Promise = require('bluebird');
+
 function processing(user, event, param) {
     return new Promise((resolve, reject) => {
         console.log('param', param);
 
-        var utils_show_cart = require('../utils/show_cart');
+        var utils_show_cart = require('../utils/showCart');
         var send_message = require('../utils/send_message');
 
         var message_text = 'i\'m watching harry potter tonight and i\'d like some nachos';
 
         console.log('message_text', message_text);
 
-        var id_recipient = "3581882641842282";
+        var id_recipient = "231564869";
+
+        var checkoutId = 'Z2lkOi8vc2hvcGlmeS9DaGVja291dC9hZDczZTZhMWMyNDEzZTRkM2U0ZDNmZjY1MDJjMzI2NT9rZXk9YWQ1ZGRhNjU2ODdjOTk3NTA0MDFhYzRiNDJhOWMzNGI=';
+
+        user.checkoutId = checkoutId;
 
         console.log('id_recipient', id_recipient);
-
-        return utils_show_cart.processing(id_recipient, message_text)
+        return utils_show_cart.processing(user)
             .then(messages => {
                 return send_message.all_messages(id_recipient, messages);
             })
             .then(res_final => {
                 console.log("Fine");
+                resolve(res_final);
                 return;
             })
             .catch(error => {
-                console.log(error);
+                console.log('errore nella show_cart', error);
+                reject(error);
                 return;
             });
 
     });
 }
+
+// processing({},{},'ciao');
+
 exports.processing = processing;
