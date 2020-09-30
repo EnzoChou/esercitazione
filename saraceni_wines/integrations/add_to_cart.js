915dad291f72f37e678ac14be35592d27a886b89
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-10-01 01:01:33 +0200

    qualcosa non va
    qualcosa non va

--- saraceni_wines/integrations/add_to_cart.js
+++ saraceni_wines/integrations/add_to_cart.js
@@ -2,8 +2,9 @@ function processing(user, event, param) {
     return new Promise((resolve, reject) => {
         console.log('param', param);
 
-        var utils_add_to_cart = require('./add_to_cart');
+        var utils_add_to_cart = require('../utils/addToCart');
         var send_message = require('../utils/send_message');
+        var controlloId = require('../utils/controlloCheckoutId');
 
         var message_text = 'i\'m watching harry potter tonight and i\'d like some nachos';
 
@@ -11,10 +12,19 @@ function processing(user, event, param) {
 
         var id_recipient = "3581882641842282";
 
+        var idVino = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNjI2MjA1MjU5MzgzMA==';
+
+        var quantity = 1;
+
         console.log('id_recipient', id_recipient);
 
-        return utils_add_to_cart.processing(id_recipient, message_text)
+        return controlloId(user.id)
+            .then(checkoutId => {
+                utils_add_to_cart(idVino, checkoutId, quantity)
+            })
             .then(messages => {
+                console.log('entrato nel lato messaggi da inviare -->',
+                messages);
                 return send_message.all_messages(id_recipient, messages);
             })
             .then(res_final => {
@@ -29,6 +39,6 @@ function processing(user, event, param) {
     });
 }
 
-processing({},{}, 'banana');
+processing({}, {}, 'banana');
 
 exports.processing = processing;
--- saraceni_wines/utils/addToCart.js
+++ saraceni_wines/utils/addToCart.js
@@ -1,3 +1,28 @@
 var Promise = require('bluebird');
 var addLineItem = require('../api/lineItemsToAdd');
+var inviaMessaggio = require('./creazioneMessaggioDiRitorno');
 
+var addToCart = function (idVino, idCheckout, quantity) {
+    var lineItemsToAdd = [{
+        variantId: idVino,
+        quantity: quantity,
+    }];
+    return new Promise((resolve, reject) => {
+        return addLineItem(idCheckout, lineItemsToAdd)
+        .then(checkout => {
+            console.log('carrello aggiornato', checkout);
+            var messaggi = ['vino inserito', 'posso fare altro per te?'];
+            return inviaMessaggio(messaggi);
+        })
+        .then(messaggioDiRitorno => {
+            resolve(messaggioDiRitorno);
+            return messaggioDiRitorno;
+        })
+        .catch(err => {
+            console.log('c\'è stato un errore nell\'addToCart', err);
+            reject(err);
+        })
+    })
+};
+
+module.exports = addToCart;
