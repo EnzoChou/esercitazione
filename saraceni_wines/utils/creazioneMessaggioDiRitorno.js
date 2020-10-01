c5d284307d286e28413c554236b6e606f4411605
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-10-01 16:19:01 +0200

    "standardizzato" messaggio di ritorno
    "standardizzato" messaggio di ritorno

--- saraceni_wines/utils/creazioneMessaggioDiRitorno.js
+++ saraceni_wines/utils/creazioneMessaggioDiRitorno.js
@@ -4,29 +4,38 @@ var creazioneMessaggioDiRitorno = function (messages) {
     return new Promise((resolve, reject) => {
         var messaggi = [];
         messages.forEach(message => {
-            if (typeof (message) === "string") {
+            console.log('\narray messaggi prima ', messaggi);
+            if (typeof (message) === "string") {                
+            console.log('\nmessaggio trovato è una stringa ---> ', message);
                 var messaggio = {};
-                messaggio.message = message;
+                messaggio.text = message;
                 messaggi.push(messaggio);
+                console.log('\narray messaggi dopo ', messaggi);
             } else {
+                console.log('\nmessaggio trovato non è una stringa ---> ', message);
                 var messaggio = {};
                 messaggio = {
                     "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "generic",
-                            "elements": []
-                        }
+                    //    "type": "template",
+                    //    "payload": {
+                    //        "template_type": "generic",
+                    //        "elements": []
+                    //    }
                     }
                 };
-                messaggio.attachment.payload.elements = message;
+                // messaggio.attachment.payload.elements = message;
+                console.log('\narray messaggi prima ', messaggi);
+                messaggio.attachment = message;
                 messaggi.push(messaggio);
+                console.log('\narray messaggi dopo ', messaggi);
             }
         })
-        console.log('\n\n\n\n\n\n\nmessaggio di ritorno --->', messaggi);
+        console.log('\n\n\n\n\n\n\creazione messaggio di ritorno --->', messaggi);
         resolve(messaggi);
         return messaggi;
     })
 };
+
+// var prova = creazioneMessaggioDiRitorno(['ciao',{}]).then(obj => console.log('oggetto ritornato ---> ', obj));
 
 module.exports = creazioneMessaggioDiRitorno;
