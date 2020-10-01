47c9bda22470f82c4142aa673193c5ccccd6cec5
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-10-01 16:18:38 +0200

    fix checkoutId
    fix checkoutId

--- saraceni_wines/utils/controlloCheckoutId.js
+++ saraceni_wines/utils/controlloCheckoutId.js
@@ -1,19 +1,28 @@
 var Promise = require('bluebird');
 var create = require('../api/create');
+var creazioneMessaggioDiRitorno = require('./creazioneMessaggioDiRitorno');
+var sendMessages = require('./send_message');
 
-var controlloCheckoutId = function(id) {
-    return new Promise ((resolve, reject) => {
-        if(!id) {
-            return create()
-            .then(checkout => {
-                resolve(checkout.id);
-                return checkout.id;
+var controlloCheckoutId = function (id) {
+    return new Promise((resolve, reject) => {
+        if (!id) {
+            return creazioneMessaggioDiRitorno()
+                .then(messages => {
+                    sendMessages(messages);
+                    return create();
+                })
+                .then(checkout => {
+                    console.log('ti ho ricreato l\'id', checkout.id);
+                    resolve(checkout.id);
+                    return checkout.id;
                 })
                 .catch(err => {
                     console.log('err nel controllo id', err)
                     reject(err);
                 })
         } else {
+            console.log('avevi già l\'id e te lo ridò uguale', id);
+            resolve(id);
             return id;
         }
     })
