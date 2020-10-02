cdcf66fa7b9ab431d0fa63909b27867ce2913c07
EnzoChou <50577526+EnzoChou@users.noreply.github.com>
2020-10-02 14:52:59 +0200

    aggiunto controllo vino su shopify
    aggiunto controllo vino su shopify

--- saraceni_wines/utils/foodpairing.js
+++ saraceni_wines/utils/foodpairing.js
@@ -1,6 +1,5 @@
 var Promise = require('bluebird');
-var fetch = require('../api/fetch');
-var checkoutFetch = require('../api/checkoutFetch');
+var shopifyApi = require('../api/shopifyApi');
 var ricercaVini = require('../script/ricercaVini');
 var creazioneMessaggioDiRitorno = require('../utils/creazioneMessaggioDiRitorno');
 
@@ -13,52 +12,57 @@ var cercaArrayVini = function (parole) {
 
 var create_foodpairing_messages = function (risultatoArrayVini, param) {
     return new Promise((resolve, reject) => {
-        var messages = [{
-            "text": "These wines look perfect for you:"
-        }, {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": []
+        if (risultatoArrayVini.length > 0) {
+            var messages = [{
+                "text": "These wines look perfect for you:"
+            }, {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": []
+                    }
                 }
-            }
 
-        }];
+            }];
 
-        var arrayDiElements = [];
+            var arrayDiElements = [];
 
-        risultatoArrayVini.forEach(vino => {
-            var postbackButton = {};
-            var buttons = [];
-            postbackButton.type = 'postback';
-            postbackButton.title = 'add 1 to cart';
-            postbackButton.payload = '000action:12345,12345||[\"add_to_cart=>' + vino.variantsId + ',1\"]000';
-            var postBackButton2 = {};
-            postBackButton2.type = 'postback';
-            postBackButton2.title = 'add 3 to cart';
-            postBackButton2.payload = '000action:12345,12345||[\"add_to_cart=>' + vino.variantsId + ',1\"]000';
-            var webUrlButton = {};
-            webUrlButton.type = 'web_url';
-            webUrlButton.url = 'www.urlToSend2Avenge.com';
-            webUrlButton.title = 'find out more';
-            buttons.push(postbackButton);
-            buttons.push(postBackButton2);
-            buttons.push(webUrlButton);
-            var element = {};
-            element.title = vino.nome;
-            element.image_url = vino.immagine;
-            element.subtitle = 'Price: ' + vino.prezzo;
-            element.buttons = buttons;
-            arrayDiElements.push(element);
-        });
-
-        messages[1].attachment.payload.elements = arrayDiElements;
-
-        resolve(messages)
-            .catch(error => {
-                reject(error);
+            risultatoArrayVini.forEach(vino => {
+                var postbackButton = {};
+                var buttons = [];
+                postbackButton.type = 'postback';
+                postbackButton.title = 'add 1 to cart';
+                postbackButton.payload = '000action:12345,12345||[\"add_to_cart=>' + vino.variants[0].id + ',1\"]000';
+                var postBackButton2 = {};
+                postBackButton2.type = 'postback';
+                postBackButton2.title = 'add 3 to cart';
+                postBackButton2.payload = '000action:12345,12345||[\"add_to_cart=>' + vino.variants[0].id + ',1\"]000';
+                var postBackButton3 = {};
+                postBackButton3.type = 'web_url';
+                postBackButton3.title = 'find out more';
+                postBackButton3.payload = '000action:12345,12345||[\"add_to_cart=>' + vino.description + ',1\"]000';
+                buttons.push(postbackButton);
+                buttons.push(postBackButton2);
+                buttons.push(webUrlButton);
+                var element = {};
+                element.title = vino.title;
+                element.image_url = vino.variants[0].image.src;
+                element.subtitle = 'Price: ' + vino.variants[0].price;
+                element.buttons = buttons;
+                arrayDiElements.push(element);
             });
+
+            messages[1].attachment.payload.elements = arrayDiElements;
+
+            resolve(messages);
+        } else {
+            var messages = ['Sorry, I haven\'t found any wines'];
+            return creazioneMessaggioDiRitorno(messages)
+            .then(messages => {
+                resolve(messages);
+            });
+        }
     });
 };
 
@@ -67,7 +71,20 @@ var create_foodpairing_messages = function (risultatoArrayVini, param) {
 function processing(text, param) {
     return cercaArrayVini(text)
         .then(risultatoArrayVini => {
-            return create_foodpairing_messages(risultatoArrayVini, param);
+            var promises = [];
+            risultatoArrayVini.forEach(vino => {
+                promises.push(shopifyApi.fetchById(vino.id));
+            })
+            return Promise.all(promises)
+                .then(arrayViniDaShopify => {
+                    if (arrayViniDaShopify && arrayViniDaShopify.length > 0) {
+                        arrayViniDaShopify = arrayViniDaShopify.filter(elem => {
+                            return elem !== undefined
+                        });
+                        console.log('\n\narrayViniTornatiDaShopify ---> ', arrayViniDaShopify);
+                        return create_foodpairing_messages(arrayViniDaShopify, param);
+                    }
+                })
         });
 }
 
